a5b847419bfe93fac1f8f4ce6b446eb6cfb5d583
axmed badri <badri736@hotmail.com>
2026-02-19 20:15:56 -0800

    modified serverjs

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ require("dotenv").config();
 const connectDB = require("./config/db")
 const app = express();
 const cors = require("cors");
+const authrouter= require("./routes/authRoutes");
+const professionalrouter = require ("./routes/professionalsRoutes");
+
+
 
 
 const _PORT = process.env.PORT;
 connectDB();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authrouter);
+app.use('/api/professionals',professionalrouter );
 
 app.listen(_PORT,()=>{
     console.log(`your server is runing at ${_PORT}`);
